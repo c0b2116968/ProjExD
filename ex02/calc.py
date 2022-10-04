a28279290e9a6560c67a1f4603a2737767be3bd6
Watabe Ryuya <c0b2116968@edu.teu.ac.jp>
2022-10-04 16:52:39 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,28 +12,56 @@ def click_equal(event):
     res = eval(eqn)
     entry.delete(0,tk.END)
     entry.insert(tk.END, res)
-    
+
+def click_del(event):
+    den = entry.delete(0,tk.END)
+
+def click_reverse(event):
+    ren = entry.get()
+    riv = int(ren) * -1
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, riv)
+
+def click_back(event):
+    br = entry.get()
+    entry.delete(0,tk.END)
+    k = br[0:-1]
+    entry.insert(tk.END,k)
 
 root = tk.Tk()
 root.geometry("300x500")
 
-entry = tk.Entry(root, width=10, font=(", 40"),justify="right")
+entry = tk.Entry(root, width=10, font=(", 40"),justify="right",bg="black",fg="white")
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
 numbers = list(range(9, -1, -1))
 operators = ["+"]
 for i, num in enumerate(numbers+operators, 1):
-    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=1)
     btn.bind("<1>",click_number)
     btn.grid(row=r, column=c)
     c += 1
     if i%3 == 0:
         r += 1
         c = 0
-btn = tk.Button(root,text=f"=", font=("", 30), width=4, height=2)
+
+btn = tk.Button(root,text=f"=", font=("", 30), width=4, height=1) 
 btn.bind("<1>",click_equal)
 btn.grid(row=r, column=c)
+
+btn = tk.Button(root,text="AC", font=("",30), width=4, height=1)
+btn.bind("<1>",click_del)
+btn.grid(row=r+1, column=0)
+
+btn = tk.Button(root,text="C", font=("",30), width=4, height=1)
+btn.bind("<1>",click_back)
+btn.grid(row=r+1, column=1)
+
+btn = tk.Button(root,text="+/-", font=("",30), width=4, height=1)
+btn.bind("<1>",click_reverse)
+btn.grid(row=r+1, column=2)
+
 
 root.mainloop()
 
