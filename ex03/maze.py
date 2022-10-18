ebdeab9f6f85f6090a83d7ce3d851e8b8a573ff2
Watabe Ryuya <c0b2116968@edu.teu.ac.jp>
2022-10-18 15:33:53 +0900

    4限目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm #8
 
 def key_down(event):
     global key
@@ -8,17 +9,30 @@ def key_up(event):
     global key
     key = ""
 
-def main_proc():  #7
-    global cx,cy
+def main_proc():
+    global cx, cy, mx, my
     if key == "Up":
-        cy -= 20
-    elif key == "Down":
-        cy += 20
-    elif key == "Left":
-        cx -= 20
-    elif key == "Right":
-        cx += 20
-    canv.coords("tori",cx,cy)
+        my -= 1
+    if key == "Down":
+        my += 1
+    if key == "Left":
+        mx -= 1
+    if key == "Right":
+        mx += 1
+    if ml[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
+
+    canv.coords("tori", cx, cy)
     root.after(100, main_proc)
 
 
@@ -26,11 +40,16 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #1
 
+    mx,my = 1,1
+
     canv = tk.Canvas(root,width=1500,height=900,bg="black")
     canv.pack()  #2
 
+    ml = mm.make_maze(15, 9) #9
+    ms = mm.show_maze(canv, ml) #10
+
     tori = tk.PhotoImage(file="fig/0.png")
-    cx,cy = 300,400
+    cx,cy = (mx*100)+50, (my*100)+50 
     canv.create_image(cx, cy, image=tori, tag="tori") #3
 
     key = "" #4
