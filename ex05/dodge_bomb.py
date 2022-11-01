14850bdd9cc35bd17aa39234b9bd32a3633db2b4
Watabe Ryuya <c0b2116968@edu.teu.ac.jp>
2022-11-01 17:58:55 +0900

    修正版 #18

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 from random import randint
 
-class Screen:
+class Screen: # 背景の設定
     def __init__(self, title, wh, pic):
         pg.display.set_caption(title)
         self.sfc = pg.display.set_mode(wh)
@@ -13,7 +13,7 @@ class Screen:
     def blit(self):
         self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
-class Bird:
+class Bird: # こうかとんのせってい
     key_delta = {
         pg.K_UP:    [0, -1],
         pg.K_DOWN:  [0, +1],
@@ -24,13 +24,13 @@ class Bird:
     def __init__(self, img, zoom, xy):
         sfc = pg.image.load(img)
         self.sfc = pg.transform.rotozoom(sfc, 0, zoom)
-        self.rct = self.sfc.get_rect()
-        self.rct.center = xy
+        self.rct = self.sfc.get_rect() 
+        self.rct.center = xy # こうかとんの位置
 
     def blit(self, scr):
         scr.sfc.blit(self.sfc, self.rct)
 
-    def update(self, scr):
+    def update(self, scr): # キーが押された時の処理
         key_states = pg.key.get_pressed()
         for key, delta in Bird.key_delta.items():
             if key_states[key]:
@@ -42,7 +42,7 @@ class Bird:
         self.blit(scr)
 
 
-class Bomb:
+class Bomb: # 爆弾の追加
     def __init__(self, color, rad, vxy, scr):
         self.sfc = pg.Surface((rad*2, rad*2)) # 空のSurface
         self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
@@ -65,12 +65,12 @@ class Bomb:
 
 class Mob: # 敵キャラの追加
     def __init__(self, img, zoom, vxy, scr):
-        sfc = pg.image.load(img)
+        sfc = pg.image.load(img) # 写真を追加
         self.sfc = pg.transform.rotozoom(sfc, 0, zoom)
         self.rct = self.sfc.get_rect()
-        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centerx = randint(0, scr.rct.width) #ランダムの位置に生成
         self.rct.centery = randint(0, scr.rct.height)
-        self.vx, self.vy = vxy
+        self.vx, self.vy = vxy #移動速度設定
 
     def blit(self, scr):
         scr.sfc.blit(self.sfc, self.rct)
@@ -89,25 +89,25 @@ class Hp: # 体力の追加
 
     def update(self, scr):
         fonto = pg.font.Font(None,100)
-        txt = fonto.render(str(f"HP{self.hp}"), True, "red") # hp表示
+        txt = fonto.render(str(f"HP{self.hp}"), True, "red") # hp表示 赤文字
         scr.sfc.blit(txt,(200,100))
 
 
-class Bomb2:
+class Bomb2: # 爆弾２つめ
     def __init__(self, color, rad, vxy, scr):
-        self.sfc = pg.Surface((rad*2, rad*2)) # 空のSurface
-        self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
-        pg.draw.circle(self.sfc, color, (rad, rad), rad) # 爆弾用の円を描く
+        self.sfc = pg.Surface((rad*2, rad*2)) 
+        self.sfc.set_colorkey((0, 0, 0)) 
+        pg.draw.circle(self.sfc, color, (rad, rad), rad)
         self.rct = self.sfc.get_rect()
-        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centerx = randint(0, scr.rct.width) #ランダムの位置に生成
         self.rct.centery = randint(0, scr.rct.height)
-        self.vx, self.vy = vxy
+        self.vx, self.vy = vxy # 移動速度
 
     def blit(self, scr):
         scr.sfc.blit(self.sfc, self.rct)
 
     def update(self, scr):
-        self.rct.move_ip(self.vx, self.vy) # 練習6
+        self.rct.move_ip(self.vx, self.vy) 
         yoko, tate = check_bound(self.rct, scr.rct)
         self.vx *= yoko
         self.vy *= tate
@@ -129,16 +129,16 @@ def check_bound(obj_rct, scr_rct):
 
 
 def main():
-    hit = 200
+    hit = 200 # 体力
 
     scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
     # 練習3
     tori = Bird("fig/6.png", 2.0, (900, 400))
     # 練習5
     bkd = Bomb((255, 0, 0), 10, (1, 1), scr)
-
+    # 敵
     mob = Mob("fig/teki.png", 2.0, (1, 1), scr)
-
+    # 爆弾2つ目
     bkd2 = Bomb((0, 255, 0), 10, (1, 1), scr)
 
     clock = pg.time.Clock() # 練習1
@@ -151,13 +151,13 @@ def main():
         tori.update(scr)
         # 練習7
         bkd.update(scr)
-
+        # 敵キャラの更新
         mob.update(scr)
-
+        # 体力の更新
         HP = Hp(hit)
         HP.update(scr)
 
-        key_states = pg.key.get_pressed()
+        key_states = pg.key.get_pressed() # spaceを押した時の処理
         if key_states[pg.K_SPACE]:
             bkd2.update(scr)
 
